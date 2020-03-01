e2d55d20f8e5b7e9129cbd2f6d39a5c3bc75f6c9
Philip Thongkhamchanh <goto.ptkc@gmail.com>
2020-03-02 10:17:11 +1100

    Update Splashscreen & Loop

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+async function splash(time) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      $("#splash").hide();
+      resolve();
+    }, time);
+  });
+}
+
+function loop() {
+  $("#paradeFloats").css({ left: -300 });
+  $("#paradeFloats").animate(
+    {
+      left: "+=850"
+    },
+    10000,
+    "linear",
+    function() {
+      loop();
+    }
+  );
+}
+
+$(document).ready(function() {
+  splash(3000).then(() => {
+    loop();
+  });
+});
